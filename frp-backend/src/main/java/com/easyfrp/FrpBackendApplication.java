dabba31f6f9b62d1e76b070e186162683cab18e7
zjx17 <zjx171@outlook.com>
2025-06-11 11:47:09 +0800

    init: create springboot backend and vue frontend

---
+++ frp-backend/src/main/java/com/easyfrp/FrpBackendApplication.java
@@ -0,0 +1,14 @@
+package com.easyfrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrpBackendApplication.class, args);
+		System.out.println("==================启动成功(╯°□°）╯︵ ┻━┻===================");
+	}
+
+}
---
+++ frp-backend/src/test/java/com/easyfrp/frp_backend/FrpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyfrp.frp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
